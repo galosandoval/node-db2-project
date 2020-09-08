518e8ae2b4a2a4ba43fb626dfd2377e42005f278
Galo Sandoval <galosan92@gmail.com>
2020-09-08 16:21:59 -0700

    finished first portion of mvp

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,11 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/car-dealer.db3'
+    },
+    useNullAsDefault: true,      //<-----needed for sqlite3
+    migrations: {               //<------places migration folder inside the data folder
+      directory: './data/migrations'
     }
   },
 
